8e6d107eb835107c5340fa0b2717c120be1ded8e
saurabhggg <39210345+saurabhggg@users.noreply.github.com>
2025-11-13 18:55:19 +0530

    Update CSE_Score filter threshold to 0.5

--- prefilterLightClassifier.py
+++ prefilterLightClassifier.py
@@ -1,5 +1,5 @@
 import pandas as pd
 df = pd.read_csv("prefilter_scored.csv")
-filtered = df[df["CSE_Score"] >= 0.45]  # or 0.5 for stricter cutoff
+filtered = df[df["CSE_Score"] >= 0.5]  # or 0.5 for stricter cutoff
 filtered.to_csv("prefilter_for_heavy.csv", index=False)
 print("✅ Filtered:", len(filtered))
